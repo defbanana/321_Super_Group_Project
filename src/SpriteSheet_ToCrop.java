f6a40aff15491489f29db08a1f0ab7a718938720
aas3084 <aas3084@CI2004-CCDG8P1.uncw.edu>
2014-11-14 15:00:35 -0500

    SpriteSheet object class

---
+++ src/SpriteSheet_ToCrop.java
@@ -0,0 +1,38 @@
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+
+
+public class SpriteSheet_ToCrop {
+	
+	BufferedImage image;
+    private Point cropLocation;
+    private Rectangle cropSize;
+	
+    
+    /**
+     * 
+     * @param i
+     * 			Provide the sprite sheet to crop from
+     * @param cropL
+     * 			x,y where we are cropping
+     * @param size
+     * 			the size of the sprite
+     */
+	public SpriteSheet_ToCrop(BufferedImage i, Point cropL,
+								Rectangle size){
+        image = i;        
+        BufferedImage clipped;       
+        cropLocation = cropL;
+        cropSize = size;
+		
+		
+								}
+	
+	
+	
+}
+
+ 
