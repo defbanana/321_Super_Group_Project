007cba7ac59bd0659fa8006302b0769672a463b2
kitsuneRose <aas3084@uncw.edu>
2014-11-17 13:35:25 -0500

    music added

---
+++ src/BGmusic.java
@@ -0,0 +1,57 @@
+
+
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+/**
+ * This class plays the background music
+ * @author team bubble
+ */
+public class BGmusic 
+{
+	/**
+	 * Plays sound file for duration of clip. Loops forever
+	 * @param fileName the filename of the .wav sound file you want to play.
+	 *
+	 */
+    public static synchronized void play(final String fileName) 
+
+    {
+
+        // Note: use .wav files             
+
+        new Thread(new Runnable() { 
+        	
+        	/**
+        	 * This is what really gets the sound playing and checks for missing/wrong files.
+        	 */
+            public void run() {
+
+                try {
+
+                    Clip clip = AudioSystem.getClip();
+
+                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(fileName));
+
+                    clip.open(inputStream);
+
+                    clip.start(); 
+                    
+                    clip.loop(Clip.LOOP_CONTINUOUSLY);  // repeat forever
+
+                } catch (Exception e) {
+
+                    System.out.println("play sound error: " + e.getMessage() + " for " + fileName);
+
+                }
+
+            }
+
+        }).start();
+
+    }
+
+}
